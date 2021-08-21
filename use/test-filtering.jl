7b83e73bd2efcf9bd0ea0c6c3ce506a3a18a8e5f
ferdiu <ferdiu.manzella@gmail.com>
2021-08-21 13:50:38 +0200

    Updated: wav filtering functions; Added: spectrogram generation; Optimization: faster animation generation (but not so fast...)

--- use/test-filtering.jl
+++ use/test-filtering.jl
@@ -1,107 +1,16 @@
 
-include("wav2stft_time_series.jl")
+include("wav-filtering.jl")
 
-# using DSP: filt
-# using Weave
-using Plots
+# TODO: find out if A1 is the highest or the lowest frequency in MFCC
 
 gr()
-
-function multibandpass_digitalfilter(vec::Vector{Tuple{T, T}}, fs::Real, window_f::Function; nbands::Integer = 60)::AbstractVector where T
-    result_filter = zeros(T, nbands)
-    @simd for t in vec
-        result_filter += digitalfilter(Filters.Bandpass(t..., fs = fs), FIRWindow(window_f(nbands)))
-    end
-    result_filter
-end
-
-function multibandpass_digitalfilter(selected_bands::Vector{Int}, fs::Real, window_f::Function; nbands::Integer = 60, lower_freq = 0)::AbstractVector
-    higher_freq = fs * 0.5
-    band_width = (higher_freq - lower_freq) / nbands
-
-    result_filter = zeros(Float64, nbands)
-    @simd for b in selected_bands
-        result_filter += digitalfilter(Filters.Bandpass(b * band_width, ((b+1) * band_width) - 1, fs = fs), FIRWindow(window_f(nbands)))
-    end
-    result_filter
-end
-
-function draw_audio_anim(
-        audio_files    :: Vector{String};
-        outfile        :: String = homedir() * "/gif.gif",
-        size           :: Tuple{Int64,Int64} = (1000, 400),
-        fps            :: Int64 = 60,
-        selected_range :: Union{UnitRange{Int64},Tuple{Number,Number},Symbol} = :whole
-    )
-    function draw_wav(points::Vector{Float64}, fs::Number; func = plot)
-        func(
-            collect(0:(length(points) - 1)),
-            points,
-            xlims = (0, length(points)),
-            ylims = (-1, 1),
-            framestyle = :zerolines,       # show axis at zeroes
-            fill = 0,                      # show area under curve
-            leg = false,                   # hide legend
-            yshowaxis = false,             # hide y axis
-            grid = false,                  # hide y grid
-            ticks = false,                 # hide y ticks
-            tick_direction = :none,
-            size = size
-        )
-    end
-    draw_wav!(points::Vector{Float64}, fs::Number) = draw_wav(points, fs, func = plot!)
-
-    @assert length(audio_files) > 0 "No audio file provided"
-
-    wavs = []
-    fss = []
-    for file_name in audio_files
-        wav, fs = wavread(file_name)
-        push!(wavs, merge_channels(wav))
-        push!(fss, fs)
-    end
-
-    @assert length(unique(fss)) == 1 "Inconsistent bitrate across multiple files"
-    @assert length(unique([x -> length(x) for wav in wavs])) == 1 "Inconsistent length across multiple files"
-
-    if selected_range isa Tuple
-        # convert seconds to points
-        println("Selected time range from $(selected_range[1])s to $(selected_range[2])s")
-        selected_range = round(Int64, selected_range[1] * fss[1]):round(Int64, selected_range[2] * fss[1])
-    end
-    # slice all wavs
-    if selected_range != :whole
-        println("Slicing from point $(collect(selected_range)[1]) to $(collect(selected_range)[end])")
-        for i in 1:length(wavs)
-            wavs[i] = wavs[i][selected_range]
-        end
-    end
-    wavlength = length(wavs[1])
-    freq = fss[1]
-    wavlength_seconds = wavlength / freq
-
-    total_frames = ceil(Int64, wavlength_seconds * fps)
-    step = wavlength / total_frames
-
-    plt = nothing
-    for (i, w) in enumerate(wavs)
-        if i == 1
-            plt = draw_wav(w, freq)
-        else
-            draw_wav!(w, freq)
-        end
-    end
-
-    anim = @animate for p in 1:total_frames
-        println("Processing frame $(p) / $(total_frames)")
-        vline!(deepcopy(plt), [ (p-1) * step ], line = (:black, 1))
-    end
-
-    gif(anim, outfile, fps = fps)
-end
+# gaston() # this uses GNUPLOT but led to some problem generating the gif
 
 # SETTINGS
-outpath = homedir()
+outpath = "filtering-results"
+if !isdir(outpath) mkpath(outpath) end
+
+selected_range = :whole
 
 inputfile_healthy = "../datasets/KDD/healthyandroidwithcough/cough/cough_9me0RMtVww_1586943699308.wav_aug_noise1.wav"
 outfile_healthy = "filtered_healthy.wav"
@@ -110,152 +19,105 @@ totaloutpath_healthy = outpath * "/" * outfile_healthy
 inputfile_covid = "../datasets/KDD/covidwebwithcough/2020-04-17-19_23_25_139694/audio_file_cough.wav"
 outfile_covid = "filtered_covid.wav"
 totaloutpath_covid = outpath * "/" * outfile_covid
+outfile_covid_single_band(feat::Int64) = string("A", feat, "_filtered_covid.wav")
+totaloutpath_covid_single_band(feat::Int64) = string(outpath, "/", outfile_covid_single_band(feat))
 
 video_codec = "libx265"
 gifout = outpath * "/" * "gif.gif"
+heatmap_png_path = outpath * "/" * "spectrogram.png"
 mkv_original = outpath * "/" * "original.mkv"
 mkv_filtered = outpath * "/" * "filtered.mkv"
+ffmpeg_output_file = outpath * "/ffmpeg-video-generation.out" # stdout
+ffmpeg_error_output_file = ffmpeg_output_file # stderr
 
 original_copy_healty = outpath * "/" * "original_healthy.wav"
 original_copy_covid = outpath * "/" * "original_covid.wav"
 
-# # READ INPUT
-# samps_healthy, sr_healthy, nbits_healthy = wavread(inputfile_healthy)
-# samps_healthy = merge_channels(samps_healthy)
+selected_features = [ 16, 17, 24, 25 ]
+nbands = 40
 
-# samps_covid, sr_covid, nbits_covid = wavread(inputfile_covid)
-# samps_covid = merge_channels(samps_covid)
+animation_fps = 30
+features_colors = [ RGB(1 - (1 * (i/(nbands - 1))), 0, 1 * (i/(nbands - 1))) for i in 0:(nbands-1) ]
 
-# # COMPUTE
-# filter_healthy = multibandpass_digitalfilter([ 48, 19, 39, 27 ], sr_healthy, hamming, nbands = 60)
-# filtered_healthy = filt(filter_healthy, samps_healthy)
+# READ INPUT
+samps_healthy, sr_healthy, nbits_healthy = wavread(inputfile_healthy)
+samps_healthy = merge_channels(samps_healthy)
+noise_gate!(samps_healthy)
+trim!(samps_healthy)
 
-# filter_covid = multibandpass_digitalfilter([ 48, 19, 39, 27 ], sr_covid, hamming, nbands = 60)
-# filtered_covid = filt(filter_covid, samps_covid)
+samps_covid, sr_covid, nbits_covid = wavread(inputfile_covid)
+samps_covid = merge_channels(samps_covid)
+noise_gate!(samps_covid)
+trim!(samps_covid)
 
-# # OUTPUT
-# cp(inputfile_healthy, original_copy_healty; force = true)
-# wavwrite(filtered_healthy, totaloutpath_healthy; Fs = sr_healthy)#, nbits = nbits_healthy)
+# COMPUTE
+filter_healthy = multibandpass_digitalfilter_mel(selected_features, sr_healthy, hamming, nbands = nbands)
+filtered_healthy = filt(filter_healthy, samps_healthy)
 
-# cp(inputfile_covid, original_copy_covid; force = true)
-# wavwrite(filtered_covid, totaloutpath_covid; Fs = sr_covid)#, nbits = nbits_covid)
+filter_covid = multibandpass_digitalfilter_mel(selected_features, sr_covid, hamming, nbands = nbands)
+filtered_covid = filt(filter_covid, samps_covid)
 
+single_band_filters = [ multibandpass_digitalfilter_mel([ feat ], sr_covid, hamming, nbands = nbands) for feat in selected_features ]
+single_band_wavs = [ (feat, totaloutpath_covid_single_band(feat), filt(single_band_filters[i], samps_covid)) for (i, feat) in enumerate(selected_features) ]
+
+# OUTPUT
+print("Copying original healthy WAV to $(original_copy_healty)...")
+wavwrite(samps_healthy, original_copy_healty; Fs = sr_healthy)
+println(" done")
+print("Generating filtered healthy WAV to $(totaloutpath_healthy)...")
+wavwrite(filtered_healthy, totaloutpath_healthy; Fs = sr_healthy)
+println(" done")
+
+print("Copying original covid WAV to $(original_copy_covid)...")
+wavwrite(samps_covid, original_copy_covid; Fs = sr_covid)
+println(" done")
+print("Generating filtered covid WAV to $(totaloutpath_covid)...")
+wavwrite(filtered_covid, totaloutpath_covid; Fs = sr_covid)
+println(" done")
+# No need to write in files the single_band_wavs samples
+# for (feat, path, wav) in single_band_wavs
+#     print("Generating filtered covid WAV to $(path)...")
+#     wavwrite(wav, path; Fs = sr_covid)
+#     println(" done")
+# end
+
+# GENERATE SPECTROGRAM
+spectrogram_plot_options = (xguide = "Time (s)", yguide = "Frequency (Hz)", ylims = (0, sr_covid / 2), clims = (-150, 0), background_color_inside = :black, size = (1600, 900))
+
+n_orig = length(samps_covid)
+nw_orig = round(Int64, n_orig / 50)
+spec_covid_original = spectrogram(samps_covid, nw_orig, round(Int64, nw_orig/2); fs = sr_covid)
+hm_orig = heatmap(spec_covid_original.time, spec_covid_original.freq, pow2db.(spec_covid_original.power); title = "Original", spectrogram_plot_options...)
+
+n_filt = length(samps_covid)
+nw_filt = round(Int64, n_filt / 50)
+spec_covid_filtered = spectrogram(filtered_covid, nw_filt, round(Int64, nw_filt/2); fs = sr_covid)
+hm_filt = heatmap(spec_covid_filtered.time, spec_covid_filtered.freq, pow2db.(spec_covid_filtered.power); title = "Filtered", spectrogram_plot_options...)
+
+plot(hm_orig, hm_filt, layout = (1, 2))
+savefig(heatmap_png_path)
+
+# GENERATE GIF
 draw_audio_anim(
-    [ original_copy_covid, totaloutpath_covid ],
+    [ (samps_covid, sr_covid), [ (single_band_wavs[i][3], sr_covid) for i in 1:length(single_band_wavs) ]... ],
+    labels = [ "Original", [ string("A", single_band_wavs[i][1]) for i in 1:length(single_band_wavs) ]... ],
+    colors = [ RGB(.3, .3, 1), features_colors[selected_features]...],
     outfile = gifout,
-    fps = 1,
-#    selected_range = (1.9, 3.15)
+    fps = animation_fps,
+    selected_range = selected_range
 )
 
+# GENERATE VIDEOS
 try
-    run(`ffmpeg -i $gifout -i $original_copy_covid -y -c:a copy -c:v $video_codec $mkv_original`)
-    run(`ffmpeg -i $gifout -i $totaloutpath_covid -y -c:a copy -c:v $video_codec $mkv_filtered`)
+    # TODO: actually Plots can generate the video directly using mp4 instead of gif at the bottom of the body of function draw_audio_anim
+    print("Generating videos in $(outpath)...")
+    run(pipeline(`ffmpeg -i $gifout -i $original_copy_covid -y -c:a copy -c:v $video_codec $mkv_original`, stdout = ffmpeg_output_file, stderr = ffmpeg_error_output_file))
+    run(pipeline(`ffmpeg -i $gifout -i $totaloutpath_covid -y -c:a copy -c:v $video_codec $mkv_filtered`, stdout = ffmpeg_output_file, stderr = ffmpeg_error_output_file))
+    println(" done")
 catch
+    println(" fail")
+    if ffmpeg_error_output_file != stderr
+        println("Look at file $(ffmpeg_error_output_file) to understand what went wrong!")
+    end
     error("unable to generate video automatially: is ffmpeg installed?")
 end
-
-# plotfilter(filter, samplerate = sr, firwindow = FIRWindow(hamming(60)))
-
-# function apply_dynfilter!(
-#         dynamic_filter::Matrix{Integer},
-#         sample::AbstractVector{T};
-#         samplerate = 16000,
-#         wintime = 0.025,
-#         steptime = 0.01,
-#         window_f::Function
-#     )::Vector{T} where {T <: Real}
-
-#     nbands = size(dynamic_filter, 2)
-#     ntimechunks = ntimechunks
-#     nwin = round(Integer, wintime * sr)
-# 	nstep = round(Integer, steptime * sr)
-
-#     window = window_f(nwin)
-
-#     winsize = (samplerate / 2) / nwin
-
-#     # init filters
-#     filters = [ digitalfilter(Filters.Bandpass((i-1) * winsize, (i * winsize) - 1, fs = samplerate), window) for i in 1:nbands ]
-
-#     # combine filters to generate EQs
-#     slice_filters = Vector{Vector{Float64}}(undef, ntimechunks)
-#     for i in 1:ntimechunks
-#         step_filters = (@view filters[findall(isequal(1), dynamic_filter[i])])
-#         slice_filters[i] = maximum.(collect(zip(step_filters...)))
-#     end
-
-#     # write filtered time chunks to new_track
-#     new_track = Vector{T}(undef, length(sample) - 1)
-#     time_chunk_length = length(sample) / ntimechunks
-#     for i in 1:ntimechunks
-#         new_track[i:(i*time_chunk_length) - 1] = filt(slice_filters[i], sample[i:(i*time_chunk_length) - 1])
-#     end
-
-#     new_track
-# end
-
-#################### FOLLOWING SECTION IS USED TO DRAW FILTERS #########################
-
-# function FIRfreqz(b::Array; w = range(0, stop=π, length=1024))::Array{ComplexF32}
-#     n = length(w)
-#     h = Array{ComplexF32}(undef, n)
-#     sw = 0
-#     for i = 1:n
-#         for j = 1:length(b)
-#         sw += b[j]*exp(-im*w[i])^-j
-#         end
-#         h[i] = sw
-#         sw = 0
-#     end
-#     h
-# end
-
-# function plotfilter(filter::Filters.Filter; samplerate = 100, firwindow = FIRWindow(hamming(100)))
-#     f = digitalfilter(filter, firwindow)
-#     w = range(0, stop=π, length=1024)
-#     h = FIRfreqz(f; w = w)
-#     ws = w / π * (samplerate / 2)
-#     plot(ws, amp2db.(abs.(h)), xlabel="Frequency(Hz)", ylabel="Magnitude(db)")
-# end
-
-# function plotfilter(filter; samplerate = 100)
-#     w = range(0, stop=π, length=1024)
-#     h = FIRfreqz(filter; w = w)
-#     ws = w / π * (samplerate / 2)
-#     plot(ws, amp2db.(abs.(h)), xlabel="Frequency(Hz)", ylabel="Magnitude(db)")
-# end
-
-# function plotfilter!(filter::Filters.Filter; samplerate = 100, firwindow = FIRWindow(hamming(100)))
-#     f = digitalfilter(filter, firwindow)
-#     w = range(0, stop=π, length=1024)
-#     h = FIRfreqz(f; w = w)
-#     ws = w / π * (samplerate / 2)
-#     plot!(ws, amp2db.(abs.(h)), xlabel="Frequency(Hz)", ylabel="Magnitude(db)")
-# end
-
-# function plotfilter!(filter; samplerate = 100)
-#     w = range(0, stop=π, length=1024)
-#     h = FIRfreqz(filter; w = w)
-#     ws = w / π * (samplerate / 2)
-#     plot!(ws, amp2db.(abs.(h)), xlabel="Frequency(Hz)", ylabel="Magnitude(db)")
-# end
-
-# myf1 = digitalfilter(Filters.Bandpass(100, 199; fs = 8000), FIRWindow(hamming(100)))
-# myf2 = digitalfilter(Filters.Bandpass(2000, 2599; fs = 8000), FIRWindow(hamming(100)))
-
-# println("myf1", myf1[1:10])
-# println("myf2", myf2[1:10])
-# myf = myf1 + myf2 #maximum.(collect(zip(myf1, myf2)))
-# println("myf", myf[1:10])
-# println(eltype(myf))
-
-# function drawnow()
-#     plotfilter(myf; samplerate = 8000)
-#     plotfilter!(myf1; samplerate = 8000)
-#     plotfilter!(myf2; samplerate = 8000)
-# end
-
-# NOTES:
-# It may be a better solution applying a bandstop filter for each "unwanted" frequency rather than
-# traying to combine multiple bandpass filters
-# They do not need to be combined but just to be applyed in sequence to each portion of the WAV file
---
+++ use/wav-filtering.jl
@@ -0,0 +1,290 @@
+
+include("wav2stft_time_series.jl")
+
+# using DSP: filt
+# using Weave
+using Plots
+using MFCC: hz2mel, mel2hz
+# using ..DecisionTree
+
+MFCC.mel2hz(f::AbstractFloat, htk=false)  = mel2hz([f], htk)[1]
+
+function FIRfreqz(b::Array; w = range(0, stop=π, length=1024))::Array{ComplexF32}
+    n = length(w)
+    h = Array{ComplexF32}(undef, n)
+    sw = 0
+    for i = 1:n
+        for j = 1:length(b)
+        sw += b[j]*exp(-im*w[i])^-j
+        end
+        h[i] = sw
+        sw = 0
+    end
+    h
+end
+
+function plotfilter(
+        filter;
+        samplerate = 100,
+        xlims::Union{Nothing,Tuple{Real,Real}} = (0, 0),
+        ylims::Union{Nothing,Tuple{Real,Real}} = (-24, 0),
+        plotfunc = plot
+    )
+    
+    w = range(0, stop=π, length=1024)
+    h = FIRfreqz(filter; w = w)
+    ws = w / π * (samplerate / 2)
+    if xlims[2] == 0
+        xlims = (xlims[1], maximum(ws))
+    end
+    plotfunc(ws, amp2db.(abs.(h)), xlabel="Frequency (Hz)", ylabel="Magnitude (db)", xlims = xlims, ylims = ylims, leg = false)
+end
+plotfilter(filter::Filters.Filter; kwargs...) = plotfilter(digitalfilter(filter, firwindow); kwargs...)
+
+plotfilter!(filter::Filters.Filter; args...) = plotfilter(filter; args..., plotfunc = plot!)
+plotfilter!(filter; args...)                 = plotfilter(filter; args..., plotfunc = plot!)
+
+function multibandpass_digitalfilter(vec::Vector{Tuple{T, T}}, fs::Real, window_f::Function; nbands::Integer = 60, nwin = nbands)::AbstractVector where T
+    result_filter = zeros(T, nbands)
+    @simd for t in vec
+        result_filter += digitalfilter(Filters.Bandpass(t..., fs = fs), FIRWindow(window_f(nwin)))
+    end
+    result_filter
+end
+
+function multibandpass_digitalfilter(selected_bands::Vector{Int}, fs::Real, window_f::Function; nbands::Integer = 60, lower_freq = 0, higher_freq = fs / 2, nwin = nbands)::AbstractVector
+    band_width = (higher_freq - lower_freq) / nbands
+
+    result_filter = zeros(Float64, nbands)
+    @simd for b in selected_bands
+        result_filter += digitalfilter(Filters.Bandpass(b * band_width, ((b+1) * band_width) - 1, fs = fs), FIRWindow(window_f(nwin)))
+    end
+    result_filter
+end
+
+function multibandpass_digitalfilter_mel(selected_bands::Vector{Int}, fs::Real, window_f::Function; nbands::Integer = 60, lower_freq = 0, higher_freq = hz2mel(fs / 2), nwin = nbands)::AbstractVector
+    correct_selected_bands = selected_bands .- 1
+    band_width = (higher_freq - lower_freq) / nbands
+
+    result_filter = zeros(Float64, nbands)
+    @simd for b in correct_selected_bands
+        result_filter += digitalfilter(Filters.Bandpass(
+            mel2hz(max(b * band_width, eps(Float64))),
+            mel2hz(((b+1) * band_width)),
+            fs = fs
+        ), FIRWindow(window_f((nwin))))
+    end
+    result_filter
+end
+
+function draw_mel_filters_graph(fs::Real, window_f::Function; nbands::Integer = 60, lower_freq = 0, higher_freq = hz2mel(fs / 2))
+    filters = [ multibandpass_digitalfilter_mel([i], fs, window_f; nbands = nbands, lower_freq = lower_freq, higher_freq = higher_freq) for i in 1:nbands ]
+    plotfilter(filters[1]; samplerate = fs)
+    for i in 2:(length(filters)-1)
+        plotfilter!(filters[i]; samplerate = fs)
+    end
+    # last call to plot has to "return" from function otherwise the graph will not be displayed
+    plotfilter!(filters[end]; samplerate = fs)
+end
+
+timerange2points(range::Tuple{T, T} where T <:Number, fs::Real)::UnitRange{Int64} = max(1, round(Int64, range[1] * fs)):round(Int64, range[2] * fs)
+
+function draw_audio_anim(
+        # TODO: figure out a way to generalize this Float64 and Float32 without getting error...
+        audio_files    :: Vector{Tuple{Vector{Float64},Float32}};
+        labels         :: Vector{String} = fill("", length(audio_files)),
+        colors         :: Union{Vector{Symbol},Vector{RGB{Float64}}} = fill(:auto, length(audio_files)),
+        outfile        :: String = homedir() * "/gif.gif",
+        size           :: Tuple{Int64,Int64} = (1000, 150 * length(audio_files)),
+        fps            :: Int64 = 60,
+        # selected_range:
+        # - 1:1000 means from point 1 to point 1000
+        # - (1.1, 2.3) means from time 1.1 to time 2.3 (in seconds)
+        # - :whole means "do not slice"
+        selected_range :: Union{UnitRange{Int64},Tuple{Number,Number},Symbol} = :whole,
+        single_graph   :: Bool = false
+    )
+    function draw_wav(points::Vector{Float64}, fs::Number; color = :auto, title = "", func = plot)
+        func(
+            collect(0:(length(points) - 1)),
+            points,
+            title = title,
+            xlims = (0, length(points)),
+            ylims = (-1, 1),
+            framestyle = :zerolines,       # show axis at zeroes
+            fill = 0,                      # show area under curve
+            leg = false,                   # hide legend
+            yshowaxis = false,             # hide y axis
+            grid = false,                  # hide y grid
+            ticks = false,                 # hide y ticks
+            tick_direction = :none,
+            linecolor = color,
+            fillcolor = color,
+            size = size
+        )
+    end
+    draw_wav!(points::Vector{Float64}, fs::Number; title = "", color = :auto) = draw_wav(points, fs, func = plot!; title = title, color = color)
+
+    @assert length(audio_files) > 0 "No audio file provided"
+    @assert length(audio_files) == length(labels) "audio_files and labels mismatch in length: $(length(audio_files)) != $(length(labels))"
+
+    wavs = []
+    fss = []
+    for f in audio_files
+        push!(wavs, merge_channels(f[1]))
+        push!(fss, f[2])
+    end
+
+    @assert length(unique(fss)) == 1 "Inconsistent bitrate across multiple files"
+    @assert length(unique([x -> length(x) for wav in wavs])) == 1 "Inconsistent length across multiple files"
+
+    if selected_range isa Tuple
+        # convert seconds to points
+        println("Selected time range from $(selected_range[1])s to $(selected_range[2])s")
+        selected_range = timerange2points(selected_range, fss[1])
+        #round(Int64, selected_range[1] * fss[1]):round(Int64, selected_range[2] * fss[1])
+    end
+    # slice all wavs
+    if selected_range != :whole
+        println("Slicing from point $(collect(selected_range)[1]) to $(collect(selected_range)[end])")
+        for i in 1:length(wavs)
+            wavs[i] = wavs[i][selected_range]
+        end
+    end
+    wavlength = length(wavs[1])
+    freq = fss[1]
+    wavlength_seconds = wavlength / freq
+
+    total_frames = ceil(Int64, wavlength_seconds * fps)
+    step = wavlength / total_frames
+
+    anim = nothing
+    plts = []
+    for (i, w) in enumerate(wavs)
+        if i == 1
+            push!(plts, draw_wav(w, freq; title = labels[i], color = colors[i]))
+        else
+            if single_graph
+                draw_wav!(w, freq; title = labels[i], color = colors[i])
+            else
+                push!(plts, draw_wav(w, freq; title = labels[i], color = colors[i]))
+            end
+        end
+    end
+
+    anim = @animate for f in 1:total_frames
+        println("Processing frame $(f) / $(total_frames)")
+        if f != 1
+            Threads.@threads for p in 1:length(plts)
+                # Make previous vline invisible
+                plts[p].series_list[end][:linealpha] = 0.0
+            end
+        end
+        #Threads.@threads 
+        for p in 1:length(plts)
+            vline!(plts[p], [ (f-1) * step ], line = (:black, 1))
+        end
+        plot(plts..., layout = (length(wavs), 1))
+    end
+
+    gif(anim, outfile, fps = fps)
+end
+
+function draw_audio_anim(audio_files::Vector{String}; kwargs...)
+    @assert length(audio_files) > 0 "No audio file provided"
+
+    converted_input = []
+    for f in audio_files
+        wav, fs = wavread(f)
+        push!(converted_input, (merge_channels(wav), fs))
+    end
+
+    draw_audio_anim(converted_input; kwargs...)
+end
+
+# struct DecisionPathNode
+#     taken         :: Bool
+#     feature       :: ModalLogic.FeatureTypeFun
+#     test_operator :: TestOperatorFun
+#     threshold     :: T where T
+#     # TODO: add here info about the world(s)
+# end
+
+# const DecisionPath = Vector{DecisionPathNode}
+
+# mutable struct InstancePathInTree
+#     file_name    :: String
+#     label        :: String
+#     tree         :: DTree{S} where S
+#     predicted    :: Union{Nothing,String}
+#     path         :: Path
+#     dataset_info :: Any
+# end
+
+# get_path_in_tree(leaf::DTLeaf, X::Any, i_instance::Integer, worlds::AbstractVector{<:AbstractWorldSet}, path::DecisionPath)::DecisionPath = path
+# function get_path_in_tree(tree::DTInternal, X::MultiFrameModalDataset, i_instance::Integer, worlds::AbstractVector{<:AbstractWorldSet}, path::DecisionPath = [])::DecisionPath
+#     satisfied = true
+# 	(satisfied,new_worlds) =
+# 		ModalLogic.modal_step(
+# 						get_frame(X, tree.i_frame),
+# 						i_instance,
+# 						worlds[tree.i_frame],
+# 						tree.relation,
+# 						tree.feature,
+# 						tree.test_operator,
+# 						tree.threshold)
+
+#     # TODO: add here info about the worlds that generated the decision
+#     push!(path, DecisionPathNode(satisfied, tree.feature, tree.test_operator, tree.threshold))
+
+# 	worlds[tree.i_frame] = new_worlds
+# 	get_path_in_tree((satisfied ? tree.left : tree.right), X, i_instance, worlds)
+# end
+# function get_path_in_tree(tree::DTree{S}, X::GenericDataset)::Vector{DecisionPath} where {S}
+# 	n_instances = n_samples(X)
+# 	paths = Vector{AbstractVector}(undef, n_instances)
+# 	for i_instance in 1:n_instances
+# 		worlds = inst_init_world_sets(X, tree, i_instance)
+# 		paths[i_instance] = apply_tree(tree.root, X, i_instance, worlds)
+# 	end
+# 	paths
+# end
+
+# TODO: implement this function for real (it is just a draft for now...)
+# function apply_dynfilter!(
+#         dynamic_filter::Matrix{Integer},
+#         sample::AbstractVector{T};
+#         samplerate = 16000,
+#         wintime = 0.025,
+#         steptime = 0.01,
+#         window_f::Function
+#     )::Vector{T} where {T <: Real}
+
+#     nbands = size(dynamic_filter, 2)
+#     ntimechunks = ntimechunks
+#     nwin = round(Integer, wintime * sr)
+#     nstep = round(Integer, steptime * sr)
+
+#     window = window_f(nwin)
+
+#     winsize = (samplerate / 2) / nwin
+
+#     # init filters
+#     filters = [ digitalfilter(Filters.Bandpass((i-1) * winsize, (i * winsize) - 1, fs = samplerate), window) for i in 1:nbands ]
+
+#     # combine filters to generate EQs
+#     slice_filters = Vector{Vector{Float64}}(undef, ntimechunks)
+#     for i in 1:ntimechunks
+#         step_filters = (@view filters[findall(isequal(1), dynamic_filter[i])])
+#         slice_filters[i] = maximum.(collect(zip(step_filters...)))
+#     end
+
+#     # write filtered time chunks to new_track
+#     new_track = Vector{T}(undef, length(sample) - 1)
+#     time_chunk_length = length(sample) / ntimechunks
+#     for i in 1:ntimechunks
+#         new_track[i:(i*time_chunk_length) - 1] = filt(slice_filters[i], sample[i:(i*time_chunk_length) - 1])
+#     end
+
+#     new_track
+# end
