bec3511a51de59c089014126187c0535ab30bd12
ferdiu <ferdiu.manzella@gmail.com>
2021-09-27 22:05:36 +0200

    refactored noise_gate!, normalize! and trim_wav!; added remove_long_silences! effect; completed splitwav and added documentation to it

--- use/wave-utils/wav-process.jl
+++ use/wave-utils/wav-process.jl
@@ -6,14 +6,14 @@ using DSP, WAV
 
 Apply noise gate to samples.
 """
-function noise_gate!(sample::AbstractVector; level::Float64 = 0.005, release::Int = 30)
-	for i in 1:(length(sample) - (release - 1))
-		v_sample = @view sample[(max(1, i - floor(Int, release/2))):(min(i + (floor(Int, release/2)), length(sample)))]
-		sample[i] = (
-			sum(abs, v_sample) / length(v_sample) <= level
-		) ? 0.0 : sample[i]
+function noise_gate!(samples::Vector{T}; level::Real = 0.005, release::Integer = 30) where T<:Number
+	for i in 1:(length(samples) - (release - 1))
+		v_samples = @view samples[(max(1, i - floor(Int, release/2))):(min(i + (floor(Int, release/2)), length(samples)))]
+		samples[i] = (
+			sum(abs, v_samples) / length(v_samples) <= level
+		) ? 0.0 : samples[i]
 	end
-	sample
+	samples
 end
 
 """
@@ -21,10 +21,11 @@ end
 
 Normalize samples to `level`.
 """
-function normalize!(sample::AbstractVector; level::Float64 = 1.0)
-	max_peak = maximum(abs, sample)
-	apply_padd!(val::Float64) = clamp((val/max_peak) * level, -1.0, 1.0)
-	sample .= apply_padd!.(sample)
+function normalize!(samples::Vector{T}; level::Real = 1.0) where T<:Number
+	max_peak = maximum(abs, samples)
+	apply_padd!(val::Real) = clamp((val/max_peak) * level, -1.0, 1.0)
+	samples .= apply_padd!.(samples)
+    samples
 end
 
 """
@@ -32,26 +33,62 @@ end
 
 Trim all trailing samples in `samples` below `level` threshold.
 """
-function trim_wav!(sample::AbstractVector; level::Float64 = 0.0)
+function trim_wav!(samples::Vector{T}; level::Real = 0.0) where T<:Number
 
-	@assert maximum(abs, sample) >= level "Level $(level) is too high or wav file is empty"
+	@assert maximum(abs, samples) >= level "Level $(level) is too high or wav file is empty"
 
 	before = 1
-	after = length(sample)
-	while before < length(sample) && abs(sample[before]) <= level
+	after = length(samples)
+	while before < length(samples) && abs(samples[before]) <= level
 		before = before + 1
 	end
-	while after > 1 && abs(sample[after]) <= level
+	while after > 1 && abs(samples[after]) <= level
 		after = after - 1
 	end
-	if after != length(sample)
-		splice!(sample, (after+1):length(sample))
+	if after != length(samples)
+		splice!(samples, (after+1):length(samples))
 	end
 	if before != 1
-		splice!(sample, 1:(before-1))
+		splice!(samples, 1:(before-1))
 	end
 
-	sample
+	samples
+end
+
+"""
+    remove_long_silences!(samples; level = 0.005, considered_long = 200)
+
+Remove long silences from WAV.
+
+NB: It is a good idea to use [`trim_wav!`](@ref) first.
+"""
+function remove_long_silences!(samples::Vector{T}; level::Real = 0.005, considered_long::Integer = 200) where T<:Number
+    under_threshold_values::Vector{Int64} = findall(s -> s <= level, samples)
+
+    i = 1
+    current_series::Vector{Int64} = []
+    intervals::Vector{UnitRange{Int64}} = []
+    while i <= length(under_threshold_values)
+        if length(current_series) > 1
+            if under_threshold_values[i] != current_series[end] + 1
+                if length(current_series) >= considered_long
+                    push!(intervals, current_series[1]:current_series[end])
+                end
+                empty!(current_series)
+            end
+
+        end
+
+        push!(current_series, under_threshold_values[i])
+
+        i += 1
+    end
+
+    for interval in reverse(intervals)
+        deleteat!(samples, interval)
+    end
+
+    samples
 end
 
 """
@@ -59,18 +96,56 @@ end
     splitwav(filepath; kwargs...)
 
 Split wav in multiple instances using a RMS-Energy based algorithm.
+
+A head will read the RMS-Energy representation of the wave
+trying to understand where the peaks are located and then
+cutting the samples accordingly.
+
+The RMS-Energy representation of the wave is built using
+overlapping frames using `wintime` and `steptime` parameters.
+
+The head sensibility can be controlled using a few parameters:
+
+* `use_last_non_peak_rms` if `true` the algorithm will try to
+determine if the peak is finished using the RMS of the last
+non-peak section of the wave.
+* `head_sensibility` controls how many RMS-frames will be
+analyzed to determine if the current peak is finished (when the
+next peak is starting).
+* `head_strictness` will determine how many RMS-sample have to
+respect the sensibility of the head.
+* `minimum_time` tells the head that a cut can not be made
+before `minimum_time` seconds (note: the granularity of this
+time depends on the `wintime` parameter).
+
+The wave can be transformed using functions:
+
+* `preprocess` (parameterizable using `preprocess_kwargs`)
+is a vector of effects that will be applied to the original
+wave before any cutting is performed.
+* `postprocess` (parameterizable using `postprocess_kwargs`)
+is a vector of effects that will be applied to the resulting
+waves, after the cutting.
 """
 function splitwav(
-            samples             :: Vector{T},
-            samplerate          :: Real;
-            wintime             :: Real                 = 0.05,
-            steptime            :: Real                 = wintime / 2,
-            head_sensibility    :: Int64                = 10,
-            preprocess          :: Vector{Function}     = Vector{Function}([ noise_gate!, normalize! ]),
-            preprocess_kwargs   :: Vector{NamedTuple}   = Vector{NamedTuple}([ (level = 0.01,), (level = 1.0,) ]),
-            postprocess         :: Vector{Function}     = Vector{Function}([ normalize!, trim_wav! ]),
-            postprocess_kwargs  :: Vector{NamedTuple}   = Vector{NamedTuple}([ (level = 1.0,), (level = 0.0,) ])
+            samples               :: Vector{T},
+            samplerate            :: Real;
+            wintime               :: Real                 = 0.05,
+            steptime              :: Real                 = wintime / 2,
+            use_last_non_peak_rms :: Bool                 = false,
+            head_sensibility      :: Int64                = 10,
+            head_strictness       :: Real                 = 0.7,
+            minimum_time          :: Real                 = 0.15,
+            preprocess            :: Vector{Function}     = Vector{Function}([ noise_gate!, normalize! ]),
+            preprocess_kwargs     :: Vector{NamedTuple}   = Vector{NamedTuple}([ (level = 0.005,), (level = 1.0,) ]),
+            postprocess           :: Vector{Function}     = Vector{Function}([ noise_gate!, normalize!, trim_wav!, remove_long_silences! ]),
+            postprocess_kwargs    :: Vector{NamedTuple}   = Vector{NamedTuple}([ (level = 0.01,), (level = 1.0,), (level = 0.0,), (level = 0.005,) ])
         )::Tuple{Vector{Vector{T}}, Real} where {T<:AbstractFloat}
+
+    if head_sensibility < 2
+        @warn "'head_sensibility` less than 2 doesn't make any sense: automatically setting it to 2."
+        head_sensibility = 2
+    end
 
     if length(preprocess) > length(preprocess_kwargs)
         append!(preprocess_kwargs, fill(NamedTuple(), length(preprocess) - length(preprocess_kwargs)))
@@ -102,19 +177,21 @@ function splitwav(
 
     cut_points::Vector{Integer} = [ 1 ]
     head_status::Symbol = :initial
-    last_read::T = Inf
+    last_read::Vector{T} = Vector{T}()
     last_non_peak_values::Vector{T} = []
     last_non_peak_rms::T = 0.0
+    first_peak_found::Int64 = 1
     for (i, f) in enumerate(frames_rms)
         if head_status == :initial
             push!(last_non_peak_values, f)
             if f > rms_f
-                last_non_peak_rms = rms(last_non_peak_values)
+                last_non_peak_rms = rms(last_non_peak_values[1:max(1,length(last_non_peak_values)-1)])
                 # now the head is reading from a peak
+                first_peak_found = i
                 head_status = :on_peak
             end
         elseif head_status == :on_peak
-            if f < rms_f && f < last_non_peak_rms
+            if f < rms_f && (!use_last_non_peak_rms || f < last_non_peak_rms)
                 # peak is over
                 head_status = :right_after_peak
             end
@@ -123,20 +200,26 @@ function splitwav(
             if f > rms_f
                 # false alarm: the peak is not yet over
                 head_status = :on_peak
-                last_read = Inf
+                empty!(last_read)
                 continue
             end
-            if last_read < f
-                # rms is start raising: time to cut
-                push!(cut_points, i)
-                # reset head status
-                head_status = :initial
-                last_read = Inf
-                last_non_peak_values = []
-                last_non_peak_rms = 0.0
-                continue
+            # start saving 'f` only if minimum_time is reached or was not set
+            if (i-first_peak_found) * steptime > minimum_time
+            # if (i-cut_points[end]) * steptime > minimum_time
+                push!(last_read, f)
+                if length(last_read) > head_sensibility
+                    popat!(last_read, 1)
+                end
+                if length(last_read) >= head_sensibility && isnondecreasing(last_read; approx = head_strictness)
+                    # rms was raising for `head_sensibility` samples: time to cut
+                    push!(cut_points, i - head_sensibility)
+                    # reset head status
+                    head_status = :initial
+                    empty!(last_read)
+                    empty!(last_non_peak_values)
+                    last_non_peak_rms = 0.0
+                end
             end
-            last_read = f
         end
     end
 
@@ -147,7 +230,8 @@ function splitwav(
 
     results::Vector{Vector{T}} = Vector{Vector{T}}(undef, length(cut_points)-1)
     for i in 2:(length(cut_points))
-        prev_cp = cut_points[i-1]
+        # assumption: always good idea to cut first frame
+        prev_cp = cut_points[i-1] + round(Int64, head_sensibility * head_strictness)
         curr_cp = cut_points[i]
         left = max(1, frame2points(prev_cp, wintime, steptime, samplerate).start)
         right = min(frame2points(curr_cp, wintime, steptime, samplerate).stop, length(samples))
@@ -177,6 +261,7 @@ end
 Create a dataset from a list of WAV files.
 
 Default values for `audio_kwargs`, `modal_args` and `data_modal_args` are:
+
     default_audio_kwargs = (
         wintime      = 0.025,
         steptime     = 0.010,
