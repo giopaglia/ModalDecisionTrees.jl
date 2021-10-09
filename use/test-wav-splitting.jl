76cbec0ace6daad9d1b50d0c8ee56740d426a5e7
ferdiu <ferdiu.manzella@gmail.com>
2021-10-09 16:09:12 +0200

    testing wav partitioning and relative drawing

--- use/test-wav-splitting.jl
+++ use/test-wav-splitting.jl
@@ -5,7 +5,29 @@ include("wav-filtering.jl")
 
 # process_dataset("KDD", found; partition_instances = true)
 
-draw_wavs_for_partitioned_dataset("KDD", "KDD-partitioned-PREPROC[]-POSTPROC[]")
+process_dataset("KDD", KDD_getSamplesList(; only_version = "c", rel_path = true);
+    out_dataset_dir_name = "KDD-norm-partitioned",
+    partition_instances = true,
+    partitioning_kwargs = (
+        cut_original        = Val(true),
+        preprocess          = Function[noise_gate!, normalize!],
+        # preprocess_kwargs   = NamedTuple[ (level = 0.005,), (level = 1.0,) ],
+        postprocess         = Function[],
+    )
+)
+
+process_dataset("KDD", KDD_getSamplesList(; only_version = "b", rel_path = true);
+    out_dataset_dir_name = "KDD-norm-partitioned",
+    partition_instances = true,
+    partitioning_kwargs = (
+        cut_original        = Val(true),
+        preprocess          = Function[normalize!],
+        # preprocess_kwargs   = NamedTuple[ (level = 0.005,), (level = 1.0,) ],
+        postprocess         = Function[],
+    )
+)
+
+draw_wavs_for_partitioned_dataset("KDD", "KDD-norm-partitioned")
 
 # generate_splitted_wavs_dataset("../datasets/KDD"; exclude = ["aug","mono","pitch"], draw_wavs = true, limit = 10)
 # generate_splitted_wavs_dataset("../datasets/KDD"; exclude = ["aug","mono","pitch","breath"], draw_wavs = true, limit = 20)
