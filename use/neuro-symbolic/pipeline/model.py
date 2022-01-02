c8eb2a5b047546190da2aae6ccf5a8a9ff5bb008
giopaglia <24519853+giopaglia@users.noreply.github.com>
2022-01-02 12:30:55 +0100

    Add results and updated neuro-pipeline

--- use/neuro-symbolic/pipeline/model.py
+++ use/neuro-symbolic/pipeline/model.py
@@ -4,7 +4,16 @@ from torch import nn
 from copy import copy
 import torch
 from torch import nn
+import random
+import os
+import numpy as np
 
+MASTER_SEED = 42
+torch.manual_seed(MASTER_SEED)
+os.environ['PYTHONHASHSEED'] = str(MASTER_SEED)
+random.seed(MASTER_SEED)
+np.random.seed(MASTER_SEED)
+torch.cuda.manual_seed_all(MASTER_SEED)
 
 class Time2Vec(nn.Module):
     def __init__(self, input_dim, embed_dim, act_function=torch.sin):
@@ -23,6 +32,7 @@ class Time2Vec(nn.Module):
 
     def forward(self, x):
         if self.enabled:
+            x = x.unsqueeze(-1)
             # size of x = [bs, sample, input_dim]
             x = torch.diag_embed(x)
             x_affine = torch.matmul(x, self.embed_weight) + self.embed_bias
@@ -37,75 +47,163 @@ class Time2Vec(nn.Module):
             x_output = x
         return x_output
 
-class TEncoder(torch.nn.Module):
-    def __init__(self, n_attributes, emb_size, code_size):
-        super(TEncoder,self).__init__()
-        # from (B,N,n_attributes) to (B,N,emb_size)
-        self.embedding_layer = Time2Vec(input_dim=n_attributes, embed_dim=emb_size)
-        self.encoder = torch.nn.TransformerEncoderLayer(d_model=emb_size, nhead=2, batch_first=True, dropout=0.25)
-        self.fc_enc = torch.nn.Linear(emb_size, code_size) # (B,L,E)
+class Decoder2(nn.Module):
+    def __init__(self, input_dim, emb_dim, hid_dim, dropout, code):
+        super().__init__()
 
-    def forward(self, x, mask):
-        # print("1", x.shape)
-        x = self.embedding_layer(x)
-        # print("2", x.shape)
-        x = self.encoder(src=x, src_key_padding_mask=mask)
-        # print("3", x.shape)
-        x = self.fc_enc(x)
-        # print("4", x.shape)
-        x = x.squeeze(0)
-        # x = x.squeeze(1)
-        # print("5", x.shape)
-        return x
+        self.hid_dim = hid_dim
+        
+        self.embedding = Time2Vec(input_dim,emb_dim)#LinearEmbedding(input_dim, emb_dim, dropout)
+        self.linear = nn.Linear(code, hid_dim)
+        self.rnn = nn.GRU(emb_dim + hid_dim, hid_dim, batch_first=True)
+        self.fc_out = nn.Linear(emb_dim + hid_dim * 2, input_dim)
 
-class TDecoder(torch.nn.Module,):
-    def __init__(self, n_attributes, emb_size, code_size):
-        super(TDecoder,self).__init__()
-        # from (B,N,n_attributes) to (B,N,emb_size)
-        self.fc_dec1 = torch.nn.Linear(code_size, emb_size) # (B,L,E)
-        self.decoder = torch.nn.TransformerEncoderLayer(d_model=emb_size, nhead=2, batch_first=True, dropout=0.25)
-        self.fc_dec2 = torch.nn.Linear(emb_size, n_attributes) # (B,L,E)
-        self.fc_dec3 = torch.nn.Linear(emb_size, n_attributes) # (B,L,E)
-
-    def forward(self, x, mask):
-        x = self.fc_dec1(x)
-        x = self.decoder(src=x, src_key_padding_mask=mask)
-        x = self.fc_dec2(x)
-        return x
+    def forward(self, src, hidden):
+        embedded = self.embedding(src)
+        context = hidden.squeeze(0).unsqueeze(1) #N
+        context = context.repeat(1,embedded.shape[1],1) #N
+        hiddens = torch.cat((context, embedded), dim=-1) #N
+        # prima prendeva embedded
+        outputs, _ = self.rnn(hiddens, hidden)#self.linear(hidden)) #no cell state!
+        
+        # outputs sono gli hidden di ogni passo
+        # il context è quello prodotto dall'encoder
+        # embedded è la target shiftata
+        #print(context.shape)
+        #print(embedded.shape)
+        #print(outputs.shape)
+        output = torch.cat((embedded, outputs, context), 
+                           dim = -1)
+        
+        return self.fc_out(output)
 
 
-class TST(torch.nn.Module):
-    def __init__(self, code_size):
-        """
-            Encoder => (B,N,n_attributes) --> (B,N,emb_size) --> (B,N,emb_size) --> (B,N,code_size)
-                                                        |
-                                                        v
-            Decoder => (B,N,code_size) --> (B,N,emb_size) --> (B,N,emb_size) --> (B,N,n_attributes)
+class Encoder(nn.Module):
+    def __init__(self, input_dim, emb_dim, hid_dim, dropout, code):
+        super().__init__()
 
-        """
-        super(TST, self).__init__()
-        n_attributes: int = 1
-        code_size: int = code_size
-        emb_size: int = 168
-        heads: int = 2
-        assert emb_size % heads == 0 and code_size % heads == 0, "Wrong number of heads"
+        self.hid_dim = hid_dim
+        
+        #self.linear = nn.Linear(hid_dim, code)
+        self.embedding = Time2Vec(input_dim, emb_dim)#LinearEmbedding(input_dim, emb_dim, dropout)
+        
+        self.rnn = nn.GRU(emb_dim, hid_dim, batch_first=True)
+        
+    def forward(self, src, mask):
+        
+        #src = [src len, batch size]
+        #print("e1", src.shape)
+        embedded = self.embedding(src)
+        
+        #print("e2", embedded.shape)
+        #embedded = [src len, batch size, emb dim]
+        packed_embedded = nn.utils.rnn.pack_padded_sequence(embedded, mask, batch_first=True,enforce_sorted=False)
+        packed_outputs, hidden = self.rnn(packed_embedded) #no cell state!
+        outputs,_ = nn.utils.rnn.pad_packed_sequence(packed_outputs, batch_first=True,total_length=embedded.shape[1])
+        #print("e3", hidden.shape)
+        #print("e1", outputs)
+        #print("------")
+        #print("h1", hidden)
+        #print("------")
+        #print("h2", hidden2)
+        #print("------")
+        ##outputs = [src len, batch size, hid dim * n directions]
+        #hidden = [n layers * n directions, batch size, hid dim]
+        
+        #outputs are always from the top hidden layer
+        
+        return hidden#self.linear(hidden)
 
-        self.encoder = TEncoder(n_attributes, emb_size, code_size)
-        self.decoder = TDecoder(n_attributes, emb_size, code_size)
-    
-    def forward(self, x, mask=None, test=False):
-        # unsqueeze added because batch_size == 1, same as removing the squeeze in the encoder
-        # print(self.encoder(x, mask).shape)
-        x = self.encoder(x, mask).unsqueeze(0)
-        # print("6", x.shape)
-        if test:
-            return x[:,0,:]
-        series_len = x.shape[1]
-        x = x[:,0,:].unsqueeze(1).repeat(1,series_len,1)
-        x = self.decoder(x,mask)
-        return x
+class Decoder(nn.Module):
+    def __init__(self, output_dim, emb_dim, hid_dim, dropout):
+        super().__init__()
 
-    @property
-    def name(self):
-        return "auto_transformers"
+        self.hid_dim = hid_dim
+        self.output_dim = output_dim
+        
+        self.embedding = LinearEmbedding(output_dim, emb_dim, dropout)
+        
+        self.rnn = nn.GRU(emb_dim + hid_dim, hid_dim, batch_first=True)
+        
+        self.fc_out = nn.Linear(emb_dim + hid_dim * 2, output_dim)
+        
+    def forward(self, input, hidden, context):
+        
+        #input = [batch size]
+        #hidden = [n layers * n directions, batch size, hid dim]
+        #context = [n layers * n directions, batch size, hid dim]
+        
+        #n layers and n directions in the decoder will both always be 1, therefore:
+        #hidden = [1, batch size, hid dim]
+        #context = [1, batch size, hid dim]
+        #hidden = hidden.squeeze().unsqueeze(1)
+        context = context.squeeze().unsqueeze(1)
 
+        #print("d1", input.shape)
+        input = input.unsqueeze(-1)
+        #print("d2", input.shape) 
+        #input = [1, batch size]
+        
+        embedded = self.embedding(input)
+        #print("d3", embedded.shape) 
+        
+        #embedded = [1, batch size, emb dim]
+                
+        emb_con = torch.cat((embedded, context), dim = 2)
+            
+        #emb_con = [1, batch size, emb dim + hid dim]
+            
+        output, hidden = self.rnn(emb_con, hidden)
+        
+        #output = [seq len, batch size, hid dim * n directions]
+        #hidden = [n layers * n directions, batch size, hid dim]
+        
+        #seq len, n layers and n directions will always be 1 in the decoder, therefore:
+        #output = [1, batch size, hid dim]
+        #hidden = [1, batch size, hid dim]
+        #print("d4", embedded.shape)
+        #print("d5", hidden.shape)
+        #print("d6", context.shape)
+
+        embedded = embedded.squeeze()
+        context = context.squeeze()
+        output = torch.cat((embedded, hidden.squeeze(0), context), 
+                           dim = 1)
+        
+        #output = [batch size, emb dim + hid dim * 2]
+        
+        prediction = self.fc_out(output)
+        
+        #prediction = [batch size, output dim]
+        #print(prediction)
+        #print("d7", prediction.shape)
+        return prediction, hidden
+
+
+class Seq2Seq(nn.Module):
+    def __init__(self, device, code, input_dim = 1, hid_dim=2, dropout=0.25, emb_dim=16):
+        super().__init__()
+        self.encoder = Encoder(input_dim, emb_dim, hid_dim, dropout, code)
+        self.decoder = Decoder2(input_dim, emb_dim, hid_dim, dropout, code)
+        self.device = device
+        
+    def forward(self, src, trg, mask, teacher_forcing_ratio = 0.5):
+        
+        #src = [src len, batch size]
+        #trg = [trg len, batch size]
+        #teacher_forcing_ratio is probability to use teacher forcing
+        #e.g. if teacher_forcing_ratio is 0.75 we use ground-truth inputs 75% of the time
+        
+        #batch_size = trg.shape[0]
+        #trg_len = trg.shape[1]
+        #trg_vocab_size = 1#self.decoder.output_dim
+        
+        #tensor to store decoder outputs
+        #outputs = torch.zeros(batch_size, trg_len, trg_vocab_size).to(self.device)
+        mask = [elem.index(0) if 0 in elem else len(elem) for elem in mask.numpy().tolist()]
+        context = self.encoder(src, mask)
+        if trg != None:
+            out = self.decoder(trg, context)
+            return out[:,:-1,:]
+        else:
+            return context.squeeze()
--- use/neuro-symbolic/pipeline/pipeline.py
+++ use/neuro-symbolic/pipeline/pipeline.py
@@ -1,134 +1,152 @@
 #!/usr/bin/python3
 import ast
 import os
-from wasabi import msg
 import pandas as pd
 import numpy as np
-from model import TST
+from model import Seq2Seq
 import torch
 #torch.use_deterministic_algorithms(True)
 
+
 model = None    # model initialization
 
-def load_model(dataset, seed, attribute, limit, code_size):
-    """ Carica modello
+def load_model(dataset, seed, attribute, code, limit):
+		""" Carica modello
+		
+		Args:
+				dataset: nome del dataset (tutto minuscolo)
+				seed: numero del seed
+				attribute: il numero dell'attributo di cui si vuole il modello (si parte da 0)
+				limit: 1 se si vogliono considerare tutti i sottointervalli
 
-    Args:
-        dataset: nome del dataset (tutto minuscolo)
-        seed: numero del seed
-        attribute: il numero dell'attributo di cui si vuole il modello (si parte da 0)
-        limit: 1 se si vogliono considerare tutti i sottointervalli
+		Returns:
+				None
+		"""
+		
+		_dataset = dataset.lower()
+		
+		if limit != 1 and limit != 100:
+				raise Exception("Limit must be 1 or 100.")
+		global model
+		device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-    Returns:
-        None
-    """
-    if limit != 1 and limit != 100:
-        raise Exception("Limit must be 1 or 100.")
-    global model
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    dataset = get_dataset(dataset)
+		path = f"assets/checkpoints/{_dataset}_code_{code}/model_code_{code}_{_dataset}_limit_{limit}_seed_{seed}_attr_{attribute}.pt"
 
-    path = f"assets/checkpoints/{dataset}-{code_size}/model_auto_transformers_{dataset}_{limit}_seed_{seed}_attr_{attribute}.pt" 
-    if not os.path.isfile(path):
-        raise Exception(f"No model found {path}. Wrong seed or dataset: !")
-    model = TST(code_size) 
-    model = torch.nn.DataParallel(model)
-    model.load_state_dict(torch.load(path, map_location=device))
-    model = model.to(device)
+		if not os.path.isfile(path):
+				raise Exception(f"No model found {path}!")
+		model = Seq2Seq(device, code, input_dim = 1, hid_dim = code, dropout=0.25, emb_dim=64) 
+		model.load_state_dict(torch.load(path, map_location=device))
+		model = model.to(device)
 
-def get_dataset(dataset):
-    """ Returns datasets' properties.
-    
-    Returns:
-        - correct dataset name
-    """
-    dataset = dataset.lower()
-    if dataset == "fingermovements":
-        return "FingerMovements"
-    if dataset == "lsst":
-        return "LSST"
-    if dataset == "libras":
-        return "Libras"
-    if dataset == "natops":
-        return "NATOPS"
-    if dataset == "racketsports":
-        return "RacketSports"
-    raise Exception("No dataset selected")
+
 
 @torch.inference_mode()
 def validation(input_serie):
-    """ Ritorna la rappresentazione di dimensione 4 per ogni istante temporale
+		""" Ritorna la rappresentazione di dimensione 4 per ogni istante temporale
 
-    Args:
-        input_serie: str, serie temporale di dimensione nx1
-    
-    """
-    global model
+		Args:
+				input_serie: str, serie temporale di dimensione 1xn
+		
+		"""
+		global model
 
-    model.eval()
-    # input_serie = ast.literal_eval(input_serie)
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    
-    with torch.no_grad():
-        input_ids = torch.tensor(input_serie).unsqueeze(0).to(device)
-        # print(input_ids)
-        # print(input_ids.shape)
-        result = model(input_ids, test=True)
-        return result.detach().cpu().numpy().tolist()[0]
+		model.eval()
+		# input_serie = ast.literal_eval(input_serie)
+		device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+		
+		with torch.no_grad():
+				input_ids = torch.tensor(input_serie).to(device)
+				mask = torch.ones(1,input_ids.shape[1])
+				result = model(input_ids, None, mask)
+				# print(result.numpy().tolist())
+				return result.numpy()
 
-# load_model("Libras", 1, 0, 1)
-load_model("RacketSports", 1, 0, 1, 2)
-print(validation([[1],[2],[3],[4.]]))
-load_model("RacketSports", 1, 0, 1, 4)
-print(validation([[1.]]))
-
-def produce_npz(dataset, seed, code_size):
+def produce_npz_flattened(dataset, seeds, code_size):
 	X = np.load(f'../neuro-symbolic-MTSC-data/stump_with_memoization,TestOp_80,{dataset},(false,false,"interval"),missing-X.npy').astype(np.float32)
 	n_points, n_attributes, n_instances = X.shape
 	print("n_points: ",     n_points)
 	print("n_attributes: ", n_attributes)
 	print("n_instances: ",  n_instances)
 
-	flattened_filename = f"flattened_{dataset}-{seed}-{code_size}-X.npy"
-	if os.path.isfile(flattened_filename):
-		print("Skipping: ", flattened_filename)
-	else:
-		flattened_array = np.ndarray((code_size,n_attributes, n_instances))
-		for attribute in range(n_attributes):
-			print(f"Attribute {attribute}/{n_attributes}")
-			load_model(dataset, seed, attribute, 100, code_size)
-			for instance in range(n_instances):
-				# print(X[:,attribute,instance].shape)
-				# print(np.expand_dims(X[:,attribute,instance], axis=0).transpose().shape)
-				flattened_array[:,attribute,instance] = validation(np.expand_dims(X[:,attribute,instance], axis=0).transpose())
-		# print(flattened_array)
-		# print(flattened_array.shape)
-		print(f"Saving {flattened_filename}...")
-		np.save(flattened_filename, flattened_array)
+	for seed in seeds:
+		flattened_filename = f"flattened_{dataset}-{seed}-{code_size}-X.npy"
+		if os.path.isfile(flattened_filename):
+			print("Skipping: ", flattened_filename)
+		else:
+			flattened_array = np.ndarray((code_size, n_attributes, n_instances))
+			for attribute in range(n_attributes):
+				print(f"Attribute {attribute}/{n_attributes}")
+				load_model(dataset, seed, attribute, code_size, 100)
+				for instance in range(n_instances):
+					# print(X[:,attribute,instance].shape)
+					# print(np.expand_dims(X[:,attribute,instance], axis=0).transpose().shape)
+					res = validation(np.expand_dims(X[:,attribute,instance], axis=0).transpose())
+					# print(res)
+					flattened_array[:,attribute,instance] = res
+			# print(flattened_array)
+			# print(flattened_array.shape)
+			print(f"Saving {flattened_filename}...")
+			np.save(flattened_filename, flattened_array)
 
-	fmd_filename = f"fmd_{dataset}-{seed}-{code_size}-X.npy"
-	if os.path.isfile(fmd_filename):
-		print("Skipping: ", fmd_filename)
-	else:
-		fmd_array = np.ndarray((code_size,n_points,n_points+1,n_attributes, n_instances))
-		for attribute in range(n_attributes):
-			print(f"Attribute {attribute}/{n_attributes}")
-			load_model(dataset, seed, attribute, 1, code_size)
-			for x in range(n_points):
-				for y in range(x+1,n_points+1):
-					for instance in range(n_instances):
-						# print(str(x)+":"+str(y))
-						# print(X[x:y,attribute,instance].shape)
-						# print(np.expand_dims(X[x:y,attribute,instance], axis=0).shape)
-						# print(np.expand_dims(X[x:y,attribute,instance], axis=0).transpose().shape)
-						# res = validation(np.expand_dims(X[x:y,attribute,instance], axis=0).transpose())
-						# print(res)
-						fmd_array[:,x,y,attribute, instance] = validation(np.expand_dims(X[x:y,attribute,instance], axis=0).transpose())
-		print(f"Saving {fmd_filename}...")
-		np.save(fmd_filename, fmd_array)
+def produce_npz_fmd(dataset, seeds, code_size):
+	X = np.load(f'../neuro-symbolic-MTSC-data/stump_with_memoization,TestOp_80,{dataset},(false,false,"interval"),missing-X.npy').astype(np.float32)
+	n_points, n_attributes, n_instances = X.shape
+	print("n_points: ",     n_points)
+	print("n_attributes: ", n_attributes)
+	print("n_instances: ",  n_instances)
 
-for code_size in [4]: # , 2]:
-	for dataset in ["RacketSports", "Libras"]: # , "FingerMovements", "LSST", "NATOPS"]:
-		for seed in [1,2,3,4,5]:
-			produce_npz(dataset, seed, code_size)
-			# break
+	for seed in seeds:
+		fmd_filename = f"fmd_{dataset}-{seed}-{code_size}-X.npy"
+		if os.path.isfile(fmd_filename):
+			print("Skipping: ", fmd_filename)
+		else:
+			fmd_array = np.ndarray((code_size,n_points,n_points+1,n_attributes, n_instances))
+			for attribute in range(n_attributes):
+				print(f"Attribute {attribute}/{n_attributes}")
+				load_model(dataset, seed, attribute, code_size, 1)
+				for x in range(n_points):
+					for y in range(x+1,n_points+1):
+						for instance in range(n_instances):
+							# print(str(x)+":"+str(y))
+							# print(X[x:y,attribute,instance].shape)
+							# print(np.expand_dims(X[x:y,attribute,instance], axis=0).shape)
+							# print(np.expand_dims(X[x:y,attribute,instance], axis=0).transpose().shape)
+							# res = validation(np.expand_dims(X[x:y,attribute,instance], axis=0).transpose())
+							# print(res)
+							res = validation(np.expand_dims(X[x:y,attribute,instance], axis=0).transpose())
+							# print(res)
+							fmd_array[:,x,y,attribute, instance] = res
+			print(f"Saving {fmd_filename}...")
+			np.save(fmd_filename, fmd_array)
+			print()
+
+################################################################################
+################################################################################
+################################################################################
+
+# load_model("Libras", 1, 0, 1)
+print("Testing a RacketSports model...")
+load_model("RacketSports", 1, 0, 1, 1)
+# load_model("RacketSports", 1, 0, 2, 1)
+print("Result:", validation([[1],[2],[3],[1],[2],[3],[4.]]))
+print()
+# load_model("RacketSports", 1, 0, 4, 1)
+print("Testing a Libras model...")
+load_model("Libras", 1, 0, 1, 1)
+print("Result:", validation([[1.]]))
+print()
+
+
+################################################################################
+################################################################################
+################################################################################
+
+for code_size in [1]: # ,2,4]:
+	for dataset in ["RacketSports", "Libras", "NATOPS"]:
+		seeds = [1,2,3,4,5]
+		produce_npz_flattened(dataset, seeds, code_size)
+
+for code_size in [1]: # ,2,4]:
+	for dataset in ["RacketSports", "Libras", "NATOPS"]:
+		seeds = [1,2,3,4,5]
+		produce_npz_fmd(dataset, seeds, code_size)
