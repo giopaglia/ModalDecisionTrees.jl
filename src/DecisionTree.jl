f2d545de25cce78f8e9122e1dc8eef6f3f7950ad
giopaglia <24519853+giopaglia@users.noreply.github.com>
2022-02-04 21:50:30 +0100

    Fix

--- src/DecisionTree.jl
+++ src/DecisionTree.jl
@@ -517,7 +517,12 @@ end
 inst_init_world_sets(X::SingleFrameGenericDataset, tree::DTree, i_instance::Integer) = 
 	inst_init_world_sets(MultiFrameModalDataset(X), tree, i_instance)
 print_apply_tree(tree::DTree{S}, X::SingleFrameGenericDataset, Y::Vector{S}; kwargs...) where {S} = 
-	print_apply_tree(tree, MultiFrameModalDataset(X), Y; kwargs...)
+    print_apply_tree(tree, MultiFrameModalDataset(X), Y; kwargs...)
+# TODO fix
+# print_apply_tree(tree::DTree{S}, X::MatricialDataset{T,D}, Y::Vector{S}; kwargs...) where {T,D,S} = begin
+#     ontology = getIntervalOntologyOfDim(Val(D-1-1))
+#     print_apply_tree(tree, MultiFrameModalDataset(OntologicalDataset{T, D-1-1, world_type(ontology)}(X)), Y; kwargs...)
+# end
 
 inst_init_world_sets(Xs::MultiFrameModalDataset, tree::DTree, i_instance::Integer) = begin
 	Ss = Vector{WorldSet}(undef, n_frames(Xs))
